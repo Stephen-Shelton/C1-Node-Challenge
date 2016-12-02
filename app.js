9ea94f6500b319dbc3b28374d2b9ba2c6d50d530
stephen-shelton <stephenshelton777@gmail.com>
2016-12-02 02:45:47 -0800

    modified package.json file. modularized bulk of request. added functionality for ignoring donuts and cc-payment transactions

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var axios = require('axios');
-var yargs = require('yargs');
 var express = require('express');
-
 var port = process.env.PORT || 3000;
 var app = express();
 
@@ -29,56 +27,11 @@ var requestConfig = {
   }
 };
 
-//criteria:
-//if amount is negative, it's spend regardless of account
-//if amount is positive, it's income regardless of account
 app.get('/summary', (req, res) => {
   axios.request(requestConfig)
     .then((response) => {
-      var data = response.data;
-      var finalJSON = {
-        average: {
-          spent: 0,
-          income: 0
-        }
-      };
-
-      data.transactions.forEach((transaction, i) => {
-        var dateStr = getDateProp(transaction);
-        var amount = transaction.amount;
-
-        //add amount to average income or average spent
-        if (amount > 0) {
-          finalJSON.average.income += amount;
-        } else {
-          finalJSON.average.spent += amount;
-        }
-
-        //add amount to month's totals
-        if (finalJSON[dateStr]) {
-          if (amount > 0) {
-            finalJSON[dateStr].income += amount;
-          } else {
-            finalJSON[dateStr].spent += amount;
-          }
-        } else {
-          finalJSON[dateStr] = {
-            spent: 0,
-            income: 0
-          };
-
-          if (amount > 0) {
-            finalJSON[dateStr].income = amount;
-          } else {
-            finalJSON[dateStr].spent = amount;
-          }
-        }
-      });
-
-      calcAverage(finalJSON);
-      amntsToStrings(finalJSON);
-      finalJSON = JSON.stringify(finalJSON);
-      res.send(finalJSON);
+      var json = assembleJSON(response.data, false, false);
+      res.send(json);
     })
     .catch((error) => {
       if (error) {
@@ -89,96 +42,58 @@ app.get('/summary', (req, res) => {
 });
 
 app.get('/ignore-donuts', (req, res) => {
-
+  axios.request(requestConfig)
+    .then((response) => {
+      var json = assembleJSON(response.data, true, false);
+      res.send(json);
+    })
+    .catch((error) => {
+      if (error) {
+        console.log(error);
+        res.send(error);
+      }
+    });
 });
 
 app.get('/ignore-cc-payments', (req, res) => {
+  axios.request(requestConfig)
+    .then((response) => {
+      var json = assembleJSON(response.data, false, true);
+      res.send(json);
+    })
+    .catch((error) => {
+      if (error) {
+        console.log(error);
+        res.send(error);
+      }
+    });
+});
 
+app.get('/ignore-donuts-and-cc-payments', (req, res) => {
+  axios.request(requestConfig)
+    .then((response) => {
+      var json = assembleJSON(response.data, true, true);
+      res.send(json);
+    })
+    .catch((error) => {
+      if (error) {
+        console.log(error);
+        res.send(error);
+      }
+    });
 });
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
 
-// Determine how much money the user spends and makes in each of the months for which we have data, and in the "average" month. What "average" means is up to you.
-
-// Output these numbers in the following format (and optionally in a more pretty format, if you see fit)
-
-//Spent criteria
-
-
-//Income criteria
-  //amount positive and account-id val is "nonce:comfy-checking/hdhehe"
-
-// {"2014-10": {"spent": "$200.00", "income": "$500.00"},
-//
-// "2014-11": {"spent": "$1510.05", "income": "$1000.00"},
-//
-// ...
-//
-// "2015-04": {"spent": "$300.00", "income": "$500.00"},
-//
-// "average": {"spent": "$750.00", "income": "$950.00"}}
-
-// CC CARD PAYMENT EXAMPLE
-// {
-//   "amount": 5194500,
-//   "is-pending": false,
-//   "aggregation-time": 1417582920000,
-//   "account-id": "nonce:comfy-cc/hdhehe",
-//   "clear-date": 1417643220000,
-//   "transaction-id": "1417643220000",
-//   "raw-merchant": "CREDIT CARD PAYMENT",
-//   "categorization": "Unknown",
-//   "merchant": "Credit Card Payment",
-//   "transaction-time": "2014-12-03T05:02:00.000Z"
-// },
-// {
-//   "amount": -5194500,
-//   "is-pending": false,
-//   "aggregation-time": 1417592640000,
-//   "account-id": "nonce:comfy-checking/hdhehe",
-//   "clear-date": 1417724460000,
-//   "transaction-id": "1417724460000",
-//   "raw-merchant": "CC PAYMENT",
-//   "categorization": "Unknown",
-//   "merchant": "CC Payment",
-//   "transaction-time": "2014-12-03T07:44:00.000Z"
-// },
-
-//INCOME EXAMPLE
-// {
-//   "amount": 17081500,
-//   "is-pending": false,
-//   "aggregation-time": 1412859120000,
-//   "account-id": "nonce:comfy-checking/hdhehe",
-//   "clear-date": 1412909700000,
-//   "transaction-id": "1412909700000",
-//   "raw-merchant": "ZENPAYROLL",
-//   "categorization": "Unknown",
-//   "merchant": "Zenpayroll",
-//   "transaction-time": "2014-10-09T12:52:00.000Z"
-// }
-
-//NEGATIVE MONEY FROM CHECKING NOT CC PAYMENT
-// {
-//   "amount": -860800,
-//   "is-pending": false,
-//   "aggregation-time": 1412764860000,
-//   "account-id": "nonce:comfy-checking/hdhehe",
-//   "clear-date": 1412948520000,
-//   "transaction-id": "1412948520000",
-//   "raw-merchant": "AT&T BILL PAYMENT",
-//   "categorization": "Unknown",
-//   "merchant": "At&T Bill Payment",
-//   "transaction-time": "2014-10-08T10:41:00.000Z"
-// },
-
 //HELPER FUNCTIONS
 function amntsToStrings(obj) {
   for (var prop in obj) {
-    obj[prop].spent = `$${Math.round(obj[prop].spent / -100)}.00`;
-    obj[prop].income = `$${Math.round(obj[prop].income / 100)}.00`;
+    if (prop !== 'ccPayments') {
+      obj[prop].spent = `$${Math.round(obj[prop].spent / -100)}.00`;
+      obj[prop].income = `$${Math.round(obj[prop].income / 100)}.00`;
+    }
   }
 }
 
@@ -189,7 +104,69 @@ function getDateProp(transaction) {
 }
 
 function calcAverage(obj) {
-  var monthsCount = Object.keys(obj).length - 1;
+  var monthsCount = (obj.ccPayments) ? Object.keys(obj).length - 2 : Object.keys(obj).length - 1;
   obj.average.spent = obj.average.spent / monthsCount;
   obj.average.income = obj.average.income / monthsCount;
+}
+
+function assembleJSON(data, ignoreDonuts, ignoreCC) {
+  var finalJSON = {
+    average: {
+      spent: 0,
+      income: 0
+    },
+    ccPayments: {}
+  };
+
+  data.transactions.forEach((transaction, i) => {
+    //case to ignore donut transactions
+    if (ignoreDonuts && (transaction.merchant === "Krispy Kreme Donuts" || transaction.merchant === "Dunkin #336784")) {
+      return;
+    }
+
+    //case to ignore cc payment transactions
+    if (ignoreCC && (transaction.merchant === "Credit Card Payment" || transaction.merchant === "CC Payment")) {
+      var id = transaction["transaction-time"];
+      finalJSON.ccPayments[id] = transaction;
+      return;
+    }
+
+    var dateStr = getDateProp(transaction);
+    var amount = transaction.amount;
+
+    //add amount to average income or average spent
+    if (amount > 0) {
+      finalJSON.average.income += amount;
+    } else {
+      finalJSON.average.spent += amount;
+    }
+
+    //add amount to month's totals
+    if (finalJSON[dateStr]) {
+      if (amount > 0) {
+        finalJSON[dateStr].income += amount;
+      } else {
+        finalJSON[dateStr].spent += amount;
+      }
+    } else {
+      finalJSON[dateStr] = {
+        spent: 0,
+        income: 0
+      };
+
+      if (amount > 0) {
+        finalJSON[dateStr].income = amount;
+      } else {
+        finalJSON[dateStr].spent = amount;
+      }
+    }
+  });
+
+  if (!ignoreCC) {
+    delete finalJSON.ccPayments;
+  }
+
+  calcAverage(finalJSON);
+  amntsToStrings(finalJSON);
+  return JSON.stringify(finalJSON);
 }
